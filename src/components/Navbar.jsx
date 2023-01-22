8e91df4fff27ac7e28ff8e08fff0a53ba0f5e459
Kristian Utsar <utsaru@gmail.com>
2023-01-22 01:13:43 +0100

    bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 import styled, { css } from "styled-components/macro";
 import { menuLinks } from "../assets/data/NavMenuLinks";
